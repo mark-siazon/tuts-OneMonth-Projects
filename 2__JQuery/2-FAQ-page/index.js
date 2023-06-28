cbef05998a7eeac7aeba28b9cf899604653f8080
Mark Angelo <markangelosiazon@gmail.com>
2023-06-28 09:27:28 +0800

    ⏏ Add | Initial Upload

---
+++ 2__JQuery/2-FAQ-page/index.js
@@ -0,0 +1,10 @@
+// $(".panel div").click(function() {
+//   var answerDiv = $(this).next(".answer");
+//   answerDiv.slideToggle("fast");
+// });
+
+
+$("#q1").click(function () { 
+  $("#a1").slideToggle();
+
+});
